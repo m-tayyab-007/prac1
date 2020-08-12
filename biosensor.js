984e0d209cf76aec4b084cc39a72457dd080d525
unknown <mtbutt@deakin.edu.au>
2020-08-12 15:20:47 +1000

    Biosensor with anonymous funtion

--- biosensor.js
+++ biosensor.js
@@ -1,1 +1,26 @@
-//created a new file
+  class Biosensor{
+      constructor(name){
+          this.person = name;
+          this.heartBeat = 0;
+          this.runLoop();
+      }
+
+      getRandomNumer(min, max) {
+        return parseInt(Math.random() * (max - min) + min);
+      }
+      
+    //   getHeartBeat(name){
+    //       let person = name;
+    //       let heartBeat = this.getHeartBeat(60,75);
+    //       console.log("Name : "+this.person + "Heart Beat : "+this.heartBeat);
+    //   }
+
+      runLoop(){
+          setInterval(()=>{
+            let heartBeat = this.getRandomNumer(60,75);
+            console.log("Name : "+this.person + "   ------   Heart Beat : "+heartBeat);
+          },1000);
+      }
+  }
+
+  let james = new Biosensor('Elle');
